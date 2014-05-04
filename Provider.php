6174508b8f2f08ee5fbeb283b8df8cd27b05ffae
mhamlet <contact@hamletmuradyan.com>
2014-05-04 19:53:30 +0400

    Adding providers' list

---
+++ Provider.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace PHPush;
+
+class Provider {
+
+    const PROVIDER_ANDROID = 'android';
+    const PROVIDER_IOS = 'ios';
+    const PROVIDER_WINDOWS_PHONE = 'windows_phone';
+    const PROVIDER_UBUNTU_PHONE = 'ubuntu_phone';
+}
---
+++ test.php
@@ -0,0 +1,7 @@
+<?php
+
+// Include PHPush
+require_once 'PHPush.php';
+
+// Creating new Android instance
+\PHPush\PHPush::Provider('android');
