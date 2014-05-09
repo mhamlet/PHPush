0e52b95f9f2e1fa1cce8c321f489828bd416246b
mhamlet <contact@hamletmuradyan.com>
2014-05-09 18:50:24 +0400

    Removing some platforms from supported list

--- Provider.php
+++ Provider.php
@@ -11,6 +11,4 @@ class Provider {
 
     const PROVIDER_ANDROID = 'android';
     const PROVIDER_IOS = 'ios';
-    const PROVIDER_WINDOWS_PHONE = 'windows_phone';
-    const PROVIDER_UBUNTU_PHONE = 'ubuntu_phone';
 }
