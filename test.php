eb88fca6be2a6855922b670a2a6d02c142aeb732
mhamlet <contact@hamletmuradyan.com>
2014-05-09 12:42:59 +0400

    Adding some test

--- test.php
+++ test.php
@@ -3,5 +3,20 @@
 // Include PHPush
 require_once 'PHPush.php';
 
-// Creating new Android instance
-\PHPush\PHPush::Provider(\PHPush\Provider::PROVIDER_ANDROID);
+// Adding Android key
+\PHPush\PHPush::Provider(\PHPush\Provider::PROVIDER_ANDROID)->setAccessKey('test');
+
+// Adding iOS certificate
+\PHPush\PHPush::Provider(\PHPush\Provider::PROVIDER_IOS)->setCertificate('ck.pem');
+
+// Creating new queue
+$queue = \PHPush\PHPush::Queue();
+
+// Adding some devices
+$queue->add(new \PHPush\providers\android\Device('adsfadsafads'));
+$queue->add(new \PHPush\providers\ios\Device('adsfadsf'));
+
+// Setting message
+$queue->message('Hello World!');
+
+$queue->send();
