1cc5176cc0fc9cb1121682a23e6b8a8482fac082
mhamlet <contact@hamletmuradyan.com>
2014-05-09 13:08:44 +0400

    Adding iOS push send code

--- providers/ios/Provider.php
+++ providers/ios/Provider.php
@@ -45,6 +45,46 @@ class Provider implements \PHPush\providers\Provider {
      * @param array  $custom_fields
      */
     public function send($message, $devices, $custom_fields = []) {
-        // TODO: Implement send() method.
+
+        // If list of devices is empty, then we must terminate this process
+        if (empty($devices)) return;
+
+        // Passphrase in default is phpush
+        $passphrase = 'pushchat';
+
+        // If we setting passphrase in custom fields, then change it and delete from array
+        if (array_key_exists('passphrase', $custom_fields)) {
+            $passphrase = $custom_fields['passphrase'];
+            unset($custom_fields['passphrase']);
+        }
+
+        $ctx = stream_context_create();
+        stream_context_set_option($ctx, 'ssl', 'local_cert', $this->certificate);
+        stream_context_set_option($ctx, 'ssl', 'passphrase', $passphrase);
+
+        // Create the payload body
+        $body['aps'] = array('alert' => $message, 'sound' => 'default');
+
+        // Encode the payload as JSON
+        $payload = json_encode($body);
+
+        // For each device
+        foreach ($devices as $device) {
+
+            // Get device token
+            $device_token = $device->getDeviceToken();
+
+            // Open stream socket
+            $fp = stream_socket_client('ssl://gateway.sandbox.push.apple.com:2195', $err, $errstr, 60, STREAM_CLIENT_CONNECT | STREAM_CLIENT_PERSISTENT, $ctx);
+
+            // Build the binary notification
+            $msg = chr(0) . pack('n', 32) . pack('H*', $device_token) . pack('n', strlen($payload)) . $payload;
+
+            // Send it to the server
+            fwrite($fp, $msg, strlen($msg));
+
+            // Close the connection to the server
+            fclose($fp);
+        }
     }
 }
