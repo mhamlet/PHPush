f05cc412c9ed877bacce5786807394deb7020471
mhamlet <contact@hamletmuradyan.com>
2014-05-09 12:03:32 +0400

    Adding devices

--- providers/ios/Device.php
+++ providers/ios/Device.php
@@ -4,6 +4,45 @@
 namespace PHPush\providers\ios;
 
 
-class Device {
+class Device implements \PHPush\providers\Device {
 
-} 
+    /**
+     * Device token
+     *
+     * @var string
+     */
+    private $device_token;
+
+    /**
+     * Setting device token
+     *
+     * @param string $device_token
+     */
+    public function __construct($device_token) {
+
+        // Saving device token
+        $this->device_token = $device_token;
+    }
+
+    /**
+     * Returns provider of device
+     *
+     * @return string
+     */
+    public function getProvider() {
+
+        // Returning android
+        return \PHPush\Provider::PROVIDER_IOS;
+    }
+
+    /**
+     * Getting device token
+     *
+     * @return string
+     */
+    public function getDeviceToken() {
+
+        // Returning device token
+        return $this->device_token;
+    }
+}
