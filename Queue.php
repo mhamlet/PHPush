cccec89afae5398407745e5e932176afc9d4bb38
mhamlet <contact@hamletmuradyan.com>
2014-05-09 12:23:20 +0400

    Adding message

--- Queue.php
+++ Queue.php
@@ -12,6 +12,20 @@ class Queue {
     private $devices = [];
 
     /**
+     * Message to send
+     *
+     * @var string
+     */
+    private $message = '';
+
+    /**
+     * Custom data to send
+     *
+     * @var array
+     */
+    private $custom_data = [];
+
+    /**
      * Add devices to queue
      *
      * @param providers\Device|array $devices
@@ -32,6 +46,25 @@ class Queue {
             // Save the device
             $this->devices[$provider][] = $device;
         }
+
+        return $this;
+    }
+
+    /**
+     * Setting message to send
+     *
+     * @param       $text
+     * @param array $custom_data
+     *
+     * @return $this
+     */
+    public function message($text, $custom_data = []) {
+
+        // Setting message
+        $this->message = $text;
+
+        // Setting custom data
+        $this->custom_data = $custom_data;
 
         return $this;
     }
